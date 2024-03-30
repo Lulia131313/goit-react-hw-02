16064756da00f02c43b315b30a8ebbea29be791a
Lulia131313 <146219985+Lulia131313@users.noreply.github.com>
2024-03-30 15:22:39 +0200

    get

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Feedback from "./components/feedback/feedback";
-import Options from "./components/options/options";
 import { useState } from "react";
+import Feedback from "./components/feedback/feedback";
+import Notification from "./components/notification/notification";
+import Options from "./components/options/options";
 
 const App = () => {
   const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
@@ -12,6 +13,15 @@ const App = () => {
     }));
   };
 
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = good + neutral + bad;
+  const positivePercentage = Math.round((good / totalFeedback) * 100);
+  console.log(positivePercentage);
+
   return (
     <>
       <h1>Sip Happens Café</h1>
@@ -19,9 +29,16 @@ const App = () => {
         Please leave your feedback about our service by selecting one of the
         options below.
       </p>
-
-      <Options updateFeedback={updateFeedback} />
-      <Feedback feedback={feedback} />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} positivePercentage={positivePercentage} />
+      ) : (
+        <Notification message="No feedback yet." />
+      )}
     </>
   );
 };
--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,10 +1,12 @@
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback, positivePercentage }) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Positive feedback percentage: {positivePercentage}%</p>
     </div>
   );
 };
+
 export default Feedback;
