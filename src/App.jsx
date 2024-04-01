8f95a11b7d018a8bf3dddf7e8697096f99762f70
Lulia131313 <146219985+Lulia131313@users.noreply.github.com>
2024-04-01 15:43:08 +0300

    get hw 02

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useEffect, useState } from "react";
 import Feedback from "./components/feedback/feedback";
 import Notification from "./components/notification/notification";
 import Options from "./components/options/options";
+import "./App.css";
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState(() => {
+    const saveFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (saveFeedback && Object.keys(saveFeedback).length) {
+      return saveFeedback;
+    }
+    return { good: 0, neutral: 0, bad: 0 };
+  });
 
   const handleFeedback = (feedbackType) => {
     setFeedback((prev) => ({
@@ -19,22 +26,15 @@ const App = () => {
 
   const { good, neutral, bad } = feedback;
   const totalFeedback = good + neutral + bad;
+  console.log(totalFeedback);
   const positivePercentage = Math.round((good / totalFeedback) * 100);
-  console.log(positivePercentage);
-
-  // useEffect(() => {
-  //   const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
-  //   if (storedFeedback) {
-  //     setFeedback(storedFeedback);
-  //   }
-  // }, []);
 
   useEffect(() => {
     localStorage.setItem("feedback", JSON.stringify(feedback));
   }, [feedback]);
 
   return (
-    <>
+    <div className="app-container">
       <h1>Sip Happens Café</h1>
       <p>
         Please leave your feedback about our service by selecting one of the
@@ -46,11 +46,15 @@ const App = () => {
         resetFeedback={resetFeedback}
       />
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} positivePercentage={positivePercentage} />
+        <Feedback
+          feedback={feedback}
+          positivePercentage={positivePercentage}
+          totalFeedback={totalFeedback}
+        />
       ) : (
         <Notification message="No feedback yet." />
       )}
-    </>
+    </div>
   );
 };
 
--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -1,9 +1,10 @@
-const Feedback = ({ feedback, positivePercentage }) => {
+const Feedback = ({ feedback, positivePercentage, totalFeedback }) => {
   return (
     <div>
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
       <p>Positive feedback percentage: {positivePercentage}%</p>
     </div>
   );
